2eda4811c7109b9d005aa79ceeb9a790047f7bbf
petrlik <petrlik142@gmail.com>
2025-02-25 16:08:58 +0100

    Add project files.

---
+++ Asp Net core web api/Controllers/RandomNumberController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp_Net_core_web_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RandomNumberController : ControllerBase
+    {
+        [HttpGet]
+        public int Get()
+        {
+            return Random.Shared.Next();
+        }
+    }
+}
---
+++ Asp Net core web api/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1",
+        new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Title = "Testing API endpoints"
+        });
+});
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.Run();
